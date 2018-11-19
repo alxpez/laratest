53e352d9910df851a52cac1f8ee704477fc0eb75
alxpez <alexperezdev@gmail.com>
2018-11-20 10:47:21 +1300

    Laracast ep 6

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,6 @@
 
 
 @section('content')
-
     <h1>HOME</h1>
-
-    <ul>
-      @foreach ($tasks as $task)
-        <li>{{ $task }}</li>
-      @endforeach
-    </ul>
-    
+    <p>{{ $foo }}</p>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -11,47 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-
-    /*
-    $tasks = [
-        'Go to work',
-        'Go Home',
-        'Repeat'
-    ];
-
-    return view('welcome', [
-        'tasks' => $tasks
-        'somethingElse' => 'RandomText'
-    ]);
-    */
-
-    // ---------- This ^^^ is same as below ---------- //
-    /*
-    $tasks = [
-        'Go to work',
-        'Go Home',
-        'Repeat'
-    ];
-
-    return view('welcome')->withTasks($tasks)->withSomethingElse('RandomText');
-    */
-
-    // ---------- This ^^^ is same as below ---------- //
-    return view('welcome').with([
-        'tasks' => [
-            'Go to work',
-            'Go Home',
-            'Repeat'
-        ],
-        'somethingElse' => 'RandomText'
-    ]);
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/contact', 'PagesController@contact');
